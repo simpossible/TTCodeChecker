5a5167cd0d8669c37e25a13fcac950f2a877cb33
simpossible <ljfsteven@163.com>
2019-07-03 14:33:01 +0800

    addfile

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup,find_packages
+
+setup(name='TTCodeChecker',
+      version="0.0.1",
+      description='oc 代码合法性检查',
+      author='simpossible',
+      author_email='https://github.com/simpossible',
+      maintainer='simpossible',
+      maintainer_email='963571744@qq.com',
+      url=' http://webpy.org/',
+      packages=find_packages(),
+      long_description="检查OC代码可能出现的问题",
+      license="MIT",
+      )
